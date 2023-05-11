f926b81301abf808c2284f5761a10a893a56e775
marhode <maximilian.rhode@capgemini.com>
2023-05-11 14:29:16 +0200

    modified wrapper script

--- k6-browser-loadtest/wrapper-script.sh
+++ k6-browser-loadtest/wrapper-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-K6_PROMETHEUS_RW_SERVER_URL= servicecenter-vminsert.servicecenterlocal \
+K6_PROMETHEUS_RW_SERVER_URL='servicecenter-vminsert.servicecenterlocal' \
 ./k6 "$@"
 
 wait -n
