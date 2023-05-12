f175d5268b5f8485c47c09a506da5fb480b8a44b
marhode <maximilian.rhode@capgemini.com>
2023-05-12 08:43:17 +0200

    Test fix for repetetive test execution

--- k6-browser-loadtest/wrapper-script.sh
+++ k6-browser-loadtest/wrapper-script.sh
@@ -4,5 +4,3 @@ K6_PROMETHEUS_RW_SERVER_URL=http://servicecenter-vminsert.servicecenterlocal/ins
 K6_PROMETHEUS_RW_USERNAME=$SECRET_USERNAME \
 K6_PROMETHEUS_RW_PASSWORD=$SECRET_PASSWORD \
 k6 "$@"
-
-wait -n
