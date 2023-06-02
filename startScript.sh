00fc5d5c0ca7299c5a1569454ff2d4f675bdc608
marhode <maximilian.rhode@capgemini.com>
2023-06-02 14:00:29 +0200

    Improved start Script for better usability

--- startScript.sh
+++ startScript.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
-#Requires a specific options File name to use as launch option
+helpFunction()
+{
+   echo ""
+   echo "Usage: ./script.sh -o optionsFile.json"
+   echo -e "\t-opt Selection of the Option File to set virtual Users and test runtime."
+   echo -e "Example: \n ./script -o 300s250u.json"
+   exit 1 # Exit script after printing help
+}
+
+
+while getopts "o:e:" opt
+do
+   case "$opt" in
+      o ) optionsParameter="$OPTARG" ;;
+      e ) envParameter="$OPTARG" ;; # For later user to change the test environment dynamically
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$optionsParameter" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
 for scenario in user-scenario; do
 	job_name=loadtest-${scenario}
 	template_cronjob_name=cronjob.batch/loadtest-${scenario}
 	
-	kubectl set env cronjob.batch/loadtest-user-scenario OPTIONS_FILE_PATH=$1 -n loadtest --kubeconfig /home/maximilian/.kube/loadtest-sc-test-loadtestdriver.yaml
-	echo "ENV changed to $1"
+	kubectl set env cronjob.batch/loadtest-user-scenario OPTIONS_FILE_PATH=$optionsParameter -n loadtest --kubeconfig /home/maximilian/.kube/loadtest-sc-test-loadtestdriver.yaml
+	echo "ENV changed to $optionsParameter"
 	kubectl create job ${job_name} -n loadtest --kubeconfig /home/maximilian/.kube/loadtest-sc-test-loadtestdriver.yaml --from=${template_cronjob_name}
 	echo "Launched job ${job_name} based on ${template_cronjob_name}"
 
