031579c3f70290919bc15b3b454c8d0fccf79b89
marhode <maximilian.rhode@capgemini.com>
2023-06-12 10:59:21 +0200

    updated wrapper script once more

--- infrastructure/k6-loadtest/wrapper-script.sh
+++ infrastructure/k6-loadtest/wrapper-script.sh
@@ -5,6 +5,8 @@ ls
 cd dbp-load-tests
 ls
 git checkout $BRANCH
+echo "Working BRANCH"
+ls
 
 K6_PROMETHEUS_RW_SERVER_URL=http://servicecenter-vminsert.servicecenterlocal/insert/0/prometheus/ \
 K6_PROMETHEUS_RW_USERNAME=$SECRET_USERNAME \
