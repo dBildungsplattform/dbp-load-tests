5f1fd81c1b88602c76dd2ccfa4b8f2945cb596be
marhode <maximilian.rhode@capgemini.com>
2023-12-07 16:49:10 +0100

    Use json stringify

--- loadtest/pages/coursePage.js
+++ loadtest/pages/coursePage.js
@@ -21,7 +21,7 @@ export default class CoursePage {
 		courseData.sesskey = sessionKey;
 		courseData.fullname = prefix + " course: " + sessionKey;
 		courseData.shortname = prefix + sessionKey;
-		console.dir("Data: " + courseData);
+		console.log("Data: " + courseData);
 		console.log("CreationURL: " + this.creationUrl);
 
 		let courseCreationRes = http.post(this.creationUrl, courseData, {
@@ -32,7 +32,7 @@ export default class CoursePage {
 		});
 
 		let urlString = courseCreationRes.url;
-		console.dir("Creation response:  " + courseCreationRes); //ERROR: creation response is https://moodle.loadtest.dbildungscloud.dev/course/edit.php
+		console.log("Creation response:  " + JSON.stringify(courseCreationRes)); //ERROR: creation response is https://moodle.loadtest.dbildungscloud.dev/course/edit.php
 		this.courseID = parseInt(urlString.match(/\d+$/));
 		this.session = sessionKey;
 		this.cookie = cookie;
